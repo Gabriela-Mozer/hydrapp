dca402da685c4acf4f2d933debbd0e6ac538084e
Gabriela Mozer <gabrielamozer1966@gmail.com>
2021-01-22 18:12:33 +0100

    index.js & delete borders

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,34 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector ('.button__add--js');
+const buttonDel = document.querySelector ('.button__del--js');
+const key = new Date().toISOString().slice(0,10);
+
+const localStorageValue = localStorage.getItem(key);
+if (localStorageValue){
+    currentGlassCounter = localStorageValue;
+}else{
+    localStorage.setItem(key, 0);
+}
+glassCounter.innerHTML = currentGlassCounter;
+
+let currentGlassCounter = 0;
+buttonAdd.addEventListener('click', () =>{
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+});
+
+buttonDel.addEventListener('click', () =>{
+    if(currentGlassCounter > 0){
+       currentGlassCounter--; 
+    }
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key,currentGlassCounter );
+});
+
+
+// if(key = '')
